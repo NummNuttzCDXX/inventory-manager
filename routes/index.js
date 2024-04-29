070c1d98c69ea3bc4b75a3e9c86aeb148b134ef1
NummNuttzCDXX <bbirdy2018@gmail.com>
2024-04-28 22:21:04 -0400

    Update Layout & Index templates & Index route - Add sidebar to Layout - Render data counts on Index route & template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+/* eslint-disable new-cap */
 const express = require('express');
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
+
+const Instruments = require('../models/Instruments');
+const Categories = require('../models/Categories');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+	// Get Counts
+	const [invCount, catCount] = await Promise.all([
+		Instruments.countDocuments({}).exec(),
+		Categories.countDocuments({}).exec(),
+	]);
+
+	res.render('index', {
+		title: 'Inventory Manager',
+		invCount,
+		catCount,
+	});
+}));
 
 module.exports = router;
